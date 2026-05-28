84f78f3c03291f6c7ca6a0ad00a1231719cdfed8
Jelle Jan Bleeker <jelle@cin-ergy.com>
2026-05-28 15:10:15 +0200

    feat: Initial Dockerfile

---
+++ startup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+if [[ ! -h /etc/caddy/Caddyfile ]]
+then
+mkdir -p /etc/caddy && cp /usr/caddy/Caddyfile /etc/caddy/Caddyfile
+fi
+
+xcaddy build $CADDY_VERSION $CADDY_MODULES
+
+caddy version
+caddy list-modules --packages --versions -s
+
+caddy run --config "/etc/caddy/Caddyfile" --adapter caddyfile
+
